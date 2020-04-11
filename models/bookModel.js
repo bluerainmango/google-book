84de85f3d84823bdbed51b1f9c8510597f2e5be0
Emily Yu <bluerainmango@gmail.com>
2020-04-11 05:22:53 -0700

    added Book model

--- models/bookModel.js
+++ models/bookModel.js
@@ -1,19 +1,24 @@
 const mongoose = require("mongoose");
 
-const bookSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: [true, "A book must have a title"],
-    trim: true,
-    maxlength: [
-      100,
-      "A book title must have less or equal than 100 characters",
-    ],
+const bookSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "A book must have a title"],
+      trim: true,
+      maxlength: [
+        100,
+        "A book title must have less or equal than 100 characters",
+      ],
+    },
+    authors: [String],
+    description: { type: String, trim: true },
+    image: [String],
+    link: { type: String, trin: true },
   },
-  authors: [String],
-  description: { type: String, trim: true },
-  image: [String],
-  link: { type: String, trin: true },
-});
+  { toJSON: { virtuals: true }, toObject: { virtuals: true } }
+);
 
-const Book = mongoose.model(bookSchema);
+const Book = mongoose.model("Book", bookSchema);
+
+module.exports = Book;
