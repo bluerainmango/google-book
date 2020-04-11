868a13cdd80f7532f4b28810bdbce5430ecac025
Emily Yu <bluerainmango@gmail.com>
2020-04-11 05:50:39 -0700

    added book APIs

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,3 +1,32 @@
-exports.getAllbooks = (req, res, next) => {};
-exports.createBook = (req, res, next) => {};
-exports.deleteBook = (req, res, next) => {};
+const Book = require("../models/bookModel");
+
+exports.getAllbooks = async (req, res, next) => {
+  const books = await Book.find();
+
+  res.status(200).json({
+    status: "success",
+    message: "Successfully got all books",
+    data: books,
+  });
+};
+
+exports.createBook = async (req, res, next) => {
+  const newBook = await Book.create(req.body);
+
+  res.status(200).json({
+    status: "success",
+    data: newBook,
+  });
+};
+
+exports.deleteBook = async (req, res, next) => {
+  const result = await Book.findByIdAndDelete(req.params.id);
+
+  if (!result) {
+    // error handling : 404, No book found with that id.
+  }
+  res.status(204).json({
+    status: "success",
+    data: null,
+  });
+};
